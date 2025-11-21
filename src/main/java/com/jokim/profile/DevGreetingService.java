72ea1e78d06a729590ef596b2d39efa34abd41a4
jok-MacBook <jokim.dev@gmail.com>
2025-11-21 11:16:05 +0900

    Fix: default로 빌드 실패하는 이슈 해결

--- src/main/java/com/jokim/profile/DevGreetingService.java
+++ src/main/java/com/jokim/profile/DevGreetingService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("dev")
+@Profile("!prod|dev")
 public class DevGreetingService implements GreetingService {
 
     @Override
