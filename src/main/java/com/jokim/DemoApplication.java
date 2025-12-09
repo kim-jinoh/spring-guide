b73c0864f305f156c21966933f8a06888bba388c
jok-MacBook <jokim.dev@gmail.com>
2025-12-09 15:46:44 +0900

    Fix: main application 설정 변경

--- src/main/java/com/jokim/DemoApplication.java
+++ src/main/java/com/jokim/DemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableScheduling
 @PropertySource("classpath:test.properties")
 public class DemoApplication {
@@ -17,12 +16,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello, %s!", name);
-    }
-
 }
 
 
